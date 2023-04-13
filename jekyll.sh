8d1e73fb7df94fbe2228a5345999ab8558651635
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2023-04-13 14:58:10 +0530

    publish port cache gems

--- jekyll.sh
+++ jekyll.sh
@@ -5,5 +5,7 @@ IFS=$'\n\t'
 export JEKYLL_VERSION=3.8
 docker run --rm \
   --volume="$PWD:/srv/jekyll:Z" \
+  --volume="jekyll-cache:/usr/local/bundle:Z" \
+  --publish 4000:4000 \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll "$@"
